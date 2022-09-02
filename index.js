1e50e36fd7d6d2f72ac0343f996d45667b658592
alexsharich <alexsharich@gmail.com>
2022-09-02 09:49:11 +0200

    add fs module

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const my_math = require('./my_math')
+/* const my_math = require('./my_math')
 const os = require('os')
 
 const currentOs = os.platform()
@@ -9,4 +9,15 @@ const res1 = my_math.add(50, 50)
 const res2 = my_math.minus(38, 1)
 
 console.log(`Result 1 : ${res1}`)
-console.log(`Result 2 : ${res2}`)
+console.log(`Result 2 : ${res2}`) */
+
+const fs = require('fs')
+
+const result = fs.readFileSync('fs_text.txt', 'utf-8')
+
+fs.writeFileSync('fs_text.txt', 'some text from fs_text')
+
+console.log(result)
+
+fs.writeFileSync('fs_text.txt', result + '\nadd new text')
+fs.writeFileSync('fs_text.txt', result + '\nadd new text')
