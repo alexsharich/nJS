ed3378f56a05adfa58d948dbfcc0c6c1ef1bb427
alexsharich <alexsharich@gmail.com>
2022-09-03 17:40:57 +0200

    add simple server

--- index.js
+++ index.js
@@ -34,8 +34,50 @@ fs.mkdir('text-files', () => {
     fs.writeFile('./text-files/some.txt', 'Hello', () => { })
 })
  */
-const fs = require('fs')
+/* const fs = require('fs')
 
 fs.unlink('./text-files/some.txt', () => {
     fs.rmdir('./text-files', () => { })
+}) */
+
+/* new server1
+const http = require('http')
+
+let server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8' })
+    res.end('hello from NODE_JS')
+})
+
+const PORT = 3000
+const HOST = 'localhost'
+
+server.listen(PORT, HOST, () => {
+    console.log(`started on  hhtp://${HOST}:${PORT}`)
+}) */
+
+//new server2
+const http = require('http')
+
+let server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' })
+    res.end(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>NODE-JS</title>
+    </head>
+    <body>
+        <h3>hello from NODE</h3>
+        <a href="google">to google</a>
+    </body>
+    </html>`)
+})
+
+const PORT = 3000
+const HOST = 'localhost'
+
+server.listen(PORT, HOST, () => {
+    console.log(`started on  hhtp://${HOST}:${PORT}`)
 })
